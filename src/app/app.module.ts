5d29eccdcf60117eb78e0e30cf3b728f95274421
mbellevue <72156423+mbellevue@users.noreply.github.com>
2022-07-18 22:24:06 -0400

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StaffComponent } from './staff/staff.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    StaffComponent
+    StaffComponent,
+    LoginComponent,
+    LogoutComponent,
+    DashboardComponent,
+    RegisterComponent,
+    HomeComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
