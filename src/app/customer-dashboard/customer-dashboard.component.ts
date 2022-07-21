d0b7228f7ed47b147ba4bd0c4ae5bde3d579ce09
Taku <takchiks@gmail.com>
2022-07-20 22:48:42 -0400

    security proxy now consumer secure:false

--- src/app/customer-dashboard/customer-dashboard.component.ts
+++ src/app/customer-dashboard/customer-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-customer-dashboard',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-dashboard.component.css']
 })
 export class CustomerDashboardComponent implements OnInit {
-
-  constructor() { }
+ben:any;
+  constructor(private customerService:CustomerService) { }
 
   ngOnInit(): void {
+    this.ben=this.customerService.getBeneficiary()
   }
 
 }
