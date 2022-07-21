c3b7c29d6a8dde2bf336e53c7149061c3697fc0e
Taku <takchiks@gmail.com>
2022-07-21 15:55:14 -0400

    services rest call 8-12 20 21 user stories

--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  baseUrl: string = "http://localhost:9090/";
+  ben: any;
+  header = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${localStorage.getItem('token')}`
+  });
+  requestOptions = { headers: this.header };
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  createStaff(staffbody:any){
+    return this.http.post(`${this.baseUrl}` + "api/admin/staff", staffbody, this.requestOptions);
+  }
+  enableStaff(enableBody:any){
+    return this.http.put(`${this.baseUrl}` + "api/admin/staff", enableBody, this.requestOptions);
+
+  }
+
 }
